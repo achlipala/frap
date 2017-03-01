ddeb7b25fadce79decc44a620f8e2396ad1adb6d
Adam Chlipala <adamc@csail.mit.edu>
2017-03-01 14:14:59 -0500

    IntroToProofScripting_template

--- IntroToProofScripting_template.v
+++ IntroToProofScripting_template.v
@@ -1,33 +1,9 @@
-(** Formal Reasoning About Programs <http://adam.chlipala.net/frap/>
-  * Supplementary Coq material: introduction to proof scripting and the Ltac language
-  * Author: Adam Chlipala
-  * License: https://creativecommons.org/licenses/by-nc-nd/4.0/
-  * Much of the material comes from CPDT <http://adam.chlipala.net/cpdt/> by the same author. *)
-
 Require Import Frap.
 
 Set Implicit Arguments.
 
 
 (** * Ltac Programming Basics *)
-
-(* We have already seen a few examples of Ltac programs, without much explanation.
- * Ltac is the proof scripting language built into Coq.  Actually, every
- * primitive step in our proofs has been a (degenerate, small) Ltac program.
- * Let's take a bottom-up look at more Ltac features.
- *
- * We've seen [match goal] tactics a few times so far.  They allow syntactic
- * inspection of hypothesis and conclusion formulas of current goals, where we
- * pick tactics to run based on what we find.  Here's a simple example to
- * find an [if] and do a case split based on its test expression. *)
-
-Ltac find_if :=
-  match goal with
-    | [ |- if ?X then _ else _ ] => cases X
-  end.
-
-(* Here's a proof that becomes trivial, given [find_if].  You can imagine a
- * whole family of similar theorems that also become trivial. *)
 
 Theorem hmm : forall (a b c : bool),
   if a
@@ -38,51 +14,12 @@ Theorem hmm : forall (a b c : bool),
       then True
       else True.
 Proof.
-  simplify.
-  repeat find_if. (* Note [repeat] for "run over and over until you can't
-                   * progress." *)
-  trivial. (* A fun tactic that consults a database of really boring steps. *)
-  trivial.
-  trivial.
-  trivial.
-
-  (* Let's write that again with more automation. *)
-  Restart.
-  simplify; repeat find_if; trivial.
-Qed.
-
-(* Another very useful Ltac building block is *context patterns*. *)
-
-Ltac find_if_inside :=
-  match goal with
-    | [ |- context[if ?X then _ else _] ] => cases X
-  end.
-
-(* The behavior of this tactic is to find any subterm of the conclusion that is
- * an [if] and then [cases] the test expression.  This version subsumes
- * [find_if].  The general behavior of [context] (an Ltac keyword) is to allow
- * matching in arbitrary subterms. *)
-
-Theorem hmm' : forall (a b c : bool),
-  if a
-    then if b
-      then True
-      else True
-    else if c
-      then True
-      else True.
-Proof.
-  simplify; repeat find_if_inside; trivial.
-Qed.
-
-(* We can also use [find_if_inside] to prove goals that [find_if] does not
- * simplify sufficiently. *)
+Admitted.
 
 Theorem hmm2 : forall (a b : bool),
   (if a then 42 else 42) = (if b then 42 else 42).
 Proof.
-  simplify; repeat find_if_inside; equality.
-Qed.
+Admitted.
 
 
 (** * Automating the two-thread locked-increment example from TransitionSystems *)
@@ -220,32 +157,14 @@ Proof.
   apply Inc2Inv; assumption.
 Qed.
 
-(* OK, HERE is where prove the main theorem.  This source file doesn't leave a
- * record of the trail of intermediate, less-automated versions, but we develop
- * it step-by-step in class. *)
+(* OK, HERE is where prove the main theorem. *)
 
 Theorem increment2_invariant_ok : invariantFor increment2_sys increment2_invariant.
 Proof.
-  apply invariant_induction; simplify;
-    repeat (match goal with
-            | [ H : parallel1 _ _ _ |- _ ] => invert H
-            | [ H : parallel2 _ _ _ _ |- _ ] => invert H
-            | [ H : increment_init _ |- _ ] => invert H
-            | [ H : increment2_invariant _ |- _ ] => invert H
-            | [ H : increment_step _ _ |- _ ] => invert H
-            | [ H : instruction_ok ?pr _ |- _ ] => cases pr
-            | [ |- increment2_invariant _ ] => apply Inc2Inv'
-            | [ |- context[shared_from_private] ] => unfold shared_from_private
-            end; simplify; try equality).
-Qed.
+Admitted.
 
 
 (** * Implementing some of [propositional] ourselves *)
-
-(* In class, we develop our own implementation of [propositional] one feature
- * at a time, but here's just the final product.  To understand it, we print
- * the definitions of the logical connectives.  Interestingly enough, they are
- * special cases of the machinery we met last time for inductive relations! *)
 
 Print True.
 Print False.
@@ -255,43 +174,15 @@ Locate "\/".
 Print or.
 (* Implication ([->]) is built into Coq, so nothing to look up there. *)
 
-Ltac my_tauto :=
-  repeat match goal with
-	   | [ H : ?P |- ?P ] => exact H
-
-	   | [ |- True ] => constructor
-	   | [ |- _ /\ _ ] => constructor
-	   | [ |- _ -> _ ] => intro
-
-	   | [ H : False |- _ ] => cases H
-	   | [ H : _ /\ _ |- _ ] => cases H
-	   | [ H : _ \/ _ |- _ ] => cases H
-
-	   | [ H1 : ?P -> ?Q, H2 : ?P |- _ ] => specialize (H1 H2)
-	 end.
-
-(* Note on some new tactics:
- * - [intro]: goes from proving [P1 -> P2] to proving [P2] with [P1] as a
- *   hypothesis.
- * - [specialize (H e1 .. eN)]: replace a hypothesis with a version that is
- *   specialized to a provided set of arguments (for quantified variables or
- *   local hypotheses from implications).  By convention, when the argument to
- *   [specialize] is an application of a hypothesis [H] to a set of arguments,
- *   the result of the specialization replaces [H]. *)
-
 Section propositional.
   Variables P Q R : Prop.
 
   Theorem propositional : (P \/ Q \/ False) /\ (P -> Q) -> True /\ Q.
   Proof.
-    my_tauto.
-  Qed.
+  Admitted.
 End propositional.
 
-(* [match goal] has useful backtracking semantics.  When one rule fails, we
- * backtrack automatically to the next one. *)
-
-(* For instance, this (unnecessarily verbose) proof script works: *)
+(* Backrtracking example #1 *)
 
 Theorem m1 : True.
 Proof.
@@ -301,81 +192,28 @@ Proof.
   end.
 Qed.
 
-(* The example shows how failure can move to a different pattern within a
- * [match].  Failure can also trigger an attempt to find _a different way of
- * matching a single pattern_.  Consider another example: *)
+(* Backtracking example #2 *)
 
 Theorem m2 : forall P Q R : Prop, P -> Q -> R -> Q.
 Proof.
   intros; match goal with
             | [ H : _ |- _ ] => idtac H
           end.
-
-  (* Coq prints "[H1]".  By applying [idtac] with an argument, a convenient
-   * debugging tool for "leaking information out of [match]es," we see that
-   * this [match] first tries binding [H] to [H1], which cannot be used to prove
-   * [Q].  Nonetheless, the following variation on the tactic succeeds at
-   * proving the goal: *)
-
-  match goal with
-    | [ H : _ |- _ ] => idtac H; exact H
-  end.
-Qed.
-
-(* The tactic first unifies [H] with [H1], as before, but [exact H] fails in
- * that case, so the tactic engine searches for more possible values of [H].
- * Eventually, it arrives at the correct value, so that [exact H] and the
- * overall tactic succeed. *)
+Admitted.
 
 (* Let's try some more ambitious reasoning, with quantifiers.  We'll be
  * instantiating quantified facts heuristically.  If we're not careful, we get
- * in a loop repeating the same instantiation forever.  We'll need a way to
- * check that a fact is not already known.  Here's a tactic: *)
+ * in a loop repeating the same instantiation forever. *)
 
-Ltac notHyp P :=
-  match goal with
-    | [ _ : P |- _ ] => fail 1
-      (* A hypothesis already asserts this fact. *)
-    | _ =>
-      match P with
-        | ?P1 /\ ?P2 =>
-          (* Check each conjunct of [P] separately, since they might be known by
-           * different means. *)
-          first [ notHyp P1 | notHyp P2 | fail 2 ]
-        | _ => idtac
-          (* If we manage to get this far, then we found no redundancy, so
-           * declare success. *)
-      end
-  end.
+(* Spec: ensure that [P] doesn't follow trivially from hypotheses. *)
+Ltac notHyp P := idtac.
 
-(* The number for [fail N] indicates failing at the backtracking point [N]
- * levels out from where we are.  [first] applies the first tactic that does not
- * fail. *)
+(* Spec: add [pf] as hypothesis only if it doesn't already follow trivially. *)
+Ltac extend pf := idtac.
 
-(* This tactic adds a fact to the context, only if it is not not already
- * present. *)
-
-Ltac extend pf :=
-  let t := type of pf in
-    notHyp t; pose proof pf.
-
-(* With these tactics defined, we can write a tactic [completer] for, among
- * other things, adding to the context all consequences of a set of simple
- * first-order formulas. *)
-
-Ltac completer :=
-  repeat match goal with
-	   | [ H : _ /\ _ |- _ ] => cases H
-           | [ H : ?P -> ?Q, H' : ?P |- _ ] => specialize (H H')
-
-           | [ H : forall x, ?P x -> _, H' : ?P ?X |- _ ] => extend (H X H')
-
-           | [ |- _ /\ _ ] => constructor
-           | [ |- forall x, _ ] => intro
-           | [ |- _ -> _ ] => intro
-             (* Interestingly, these last two rules are redundant.
-              * See CPDT for details.... *)
-         end.
+(* Spec: add all simple consequences of known facts, including
+ * [forall]-quantified. *)
+Ltac completer := idtac.
 
 Section firstorder.
   Variable A : Set.
@@ -386,102 +224,30 @@ Section firstorder.
 
   Theorem fo : forall (y x : A), P x -> S x.
   Proof.
-    completer.
-    assumption.
-  Qed.
+  Admitted.
 End firstorder.
 
 
 (** * Functional Programming in Ltac *)
 
-(* Let's write a list-length function in Ltac rather than Gallina.  In class,
- * we'll muddle through some intermediate versions before getting to the first
- * version that at least parses. *)
-
-Module Import FirstTry.
-  Ltac length ls :=
-    match ls with
-    | nil => O
-    | _ :: ?ls' => constr:(S (length ls'))
-    end.
-End FirstTry.
+(* Spec: return length of list. *)
+Ltac length ls := constr:(0).
 
 Goal False.
   let n := length (1 :: 2 :: 3 :: nil) in
     pose n.
 Abort.
 
-(* Something went wrong there. *)
-
-Ltac length ls :=
-  match ls with
-    | nil => O
-    | _ :: ?ls' =>
-      let ls'' := length ls' in
-        constr:(S ls'')
-  end.
+(* Spec: map Ltac function over list. *)
+Ltac map f ls := constr:(0).
 
 Goal False.
-  let n := length (1 :: 2 :: 3 :: nil) in
-    pose n.
-Abort.
-
-(* Here's a [map] implementation in Ltac.  Strangely, it needs to be passed the
- * type of the new list explicitly. *)
-
-Ltac map T f :=
-  let rec map' ls :=
-    match ls with
-      | nil => constr:(@nil T)
-      | ?x :: ?ls' =>
-        let x' := f x in
-          let ls'' := map' ls' in
-            constr:(x' :: ls'')
-    end in
-  map'.
-
-Goal False.
-  let ls := map (nat * nat)%type ltac:(fun x => constr:((x, x))) (1 :: 2 :: 3 :: nil) in
-    pose ls.
+  (*let ls := map (nat * nat)%type ltac:(fun x => constr:((x, x))) (1 :: 2 :: 3 :: nil) in
+    pose ls.*)
 Abort.
 
 (* Now let's revisit [length] and see how we might implement "printf debugging"
  * for it. *)
-
-Module Import WithPrinting.
-  Ltac length ls :=
-    idtac ls;
-    match ls with
-    | nil => O
-    | _ :: ?ls' =>
-      let ls'' := length ls' in
-      constr:(S ls'')
-    end.
-End WithPrinting.
-
-Goal False.
-  (*let n := length (1 :: 2 :: 3 :: nil) in
-    pose n.*)
-  (* Oh, that has a dynamic type error. *)
-Abort.
-
-(* The problem is that Ltac as a language contains several datatypes.  One of
- * them is "tactic sequence," which can't be mixed with other datatypes like
- * "term in the logic."  Tactic sequences don't return results.  We can use
- * continuation-passing style as a mitigation. *)
-
-Module Import WithPrintingFixed.
-  Ltac length ls k :=
-    idtac ls;
-    match ls with
-    | nil => k O
-    | _ :: ?ls' => length ls' ltac:(fun n => k (S n))
-    end.
-End WithPrintingFixed.
-
-Goal False.
-  length (1 :: 2 :: 3 :: nil) ltac:(fun n => pose n).
-Abort.
 
 
 (** * Recursive Proof Search *)
@@ -489,18 +255,7 @@ Abort.
 (* Let's work on a tactic to try all possible instantiations of quantified
  * hypotheses, attempting to find out where the goal becomes obvious. *)
 
-Ltac inster n :=
-  intuition; (* <-- A fancier version of [propositional] whose details we won't
-              *     dwell on *)
-    match n with
-      | S ?n' =>
-        match goal with
-          | [ H : forall x : ?T, _, y : ?T |- _ ] => pose proof (H y); inster n'
-        end
-    end.
-
-(* Important: when one recursive call fails, the backtracking semantics of
- * [match goal] cause us to try the next instantiation! *)
+Ltac inster n := idtac.
 
 Section test_inster.
   Variable A : Set.
@@ -513,7 +268,7 @@ Section test_inster.
   Theorem test_inster : forall x, P (g x x) -> Q (f x).
   Proof.
     inster 2.
-  Qed.
+  Admitted.
 
   Hypothesis H3 : forall u v, P u /\ P v /\ u <> v -> P (g u v).
   Hypothesis H4 : forall u, Q (f u) -> P u /\ P (f u).
@@ -521,7 +276,7 @@ Section test_inster.
   Theorem test_inster2 : forall x y, x <> y -> P x -> Q (f y) -> Q (f x).
   Proof.
     inster 3.
-  Qed.
+  Admitted.
 End test_inster.
 
 (** ** A fancier example of proof search (probably skipped on first
@@ -717,24 +472,15 @@ Proof.
   apply H.
 Qed.
 
-Ltac newEvar T k :=
-  let x := fresh "x" in
-  evar (x : T);
-  let x' := eval unfold x in x in
-    clear x; k x'.
+(* Spec: create new evar of type [T] and pass to [k]. *)
+Ltac newEvar T k := idtac.
 
-Ltac insterU H :=
-  repeat match type of H with
-           | forall x : ?T, _ =>
-             newEvar T ltac:(fun y => specialize (H y))
-         end.
+(* Spec: instantiate initial [forall]s of [H] with new evars. *)
+Ltac insterU H := idtac.
 
 Theorem t5' : (forall x : nat, S x > x) -> 2 > 1.
 Proof.
-  intro H.
-  insterU H.
-  apply H.
-Qed.
+Admitted.
 
 (* This particular example is somewhat silly, since [apply] by itself would have
  * solved the goal originally.  Separate forward reasoning is more useful on
@@ -742,9 +488,7 @@ Qed.
  * example, it is useful to define a variant of [insterU] that does not clear
  * the base hypothesis we pass to it. *)
 
-Ltac insterKeep H :=
-  let H' := fresh "H'" in
-    pose proof H as H'; insterU H'.
+Ltac insterKeep H := idtac.
 
 Section t6.
   Variables A B : Type.
@@ -760,22 +504,7 @@ Section t6.
 
   Theorem t6 : forall v1 v2, exists u1, exists u2, P (f v1 v2) (g u1 u2).
   Proof.
-    intros.
-
-    do 2 insterKeep H1.
-
-    repeat match goal with
-             | [ H : ex _ |- _ ] => destruct H
-           end.
-
-    eexists.
-    eexists.
-    apply H2.
-    exact H.
-    exact p.
-    (* In two weeks, we'll meet [eauto], which can do these last steps
-     * automatically. *)
-  Qed.
+  Admitted.
 End t6.
 
 (* Here's an example where something bad happens. *)
@@ -795,7 +524,7 @@ Section t7.
 
   Theorem t7 : forall v1 v2, Q v1 -> Q v2 -> exists u1, exists u2, P (f v1 v2) (g u1 u2).
   Proof.
-    intros; do 2 insterKeep H1;
+    (*intros; do 2 insterKeep H1;
       repeat match goal with
                | [ H : ex _ |- _ ] => destruct H
              end; eauto.
@@ -803,61 +532,9 @@ Section t7.
     (* Oh, two trivial goals remain. *)
     Unshelve.
     assumption.
-    assumption.
-  Qed.
+    assumption.*)
+  Admitted.
 End t7.
-
-(* Why did we need to do that extra work?  The [forall] rule was also matching
- * implications! *)
-
-Module Import FixedInster.
-  Ltac insterU tac H :=
-    repeat match type of H with
-           | forall x : ?T, _ =>
-             match type of T with
-             | Prop =>
-               (let H' := fresh "H'" in
-                assert (H' : T) by solve [ tac ];
-                specialize (H H'); clear H')
-               || fail 1
-             | _ =>
-               newEvar T ltac:(fun y => specialize (H y))
-             end
-           end.
-
-  Ltac insterKeep tac H :=
-    let H' := fresh "H'" in
-      pose proof H as H'; insterU tac H'.
-End FixedInster.
-
-Section t7'.
-  Variables A B : Type.
-  Variable Q : A -> Prop.
-  Variable P : A -> B -> Prop.
-  Variable f : A -> A -> A.
-  Variable g : B -> B -> B.
-
-  Hypothesis H1 : forall v, Q v -> exists u, P v u.
-  Hypothesis H2 : forall v1 u1 v2 u2,
-    P v1 u1
-    -> P v2 u2
-    -> P (f v1 v2) (g u1 u2).
-
-  Theorem t7' : forall v1 v2, Q v1 -> Q v2 -> exists u1, exists u2, P (f v1 v2) (g u1 u2).
-  Proof.
-    intros.
-    do 2 insterKeep ltac:(idtac; match goal with
-                                 | [ H : Q ?v |- _ ] =>
-                                   match goal with
-                                   | [ _ : context[P v _] |- _ ] => fail 1
-                                   | _ => apply H
-                                   end
-                                 end) H1;
-    repeat match goal with
-           | [ H : ex _ |- _ ] => destruct H
-           end; eauto.
-  Qed.
-End t7'.
 
 Theorem t8 : exists p : nat * nat, fst p = 3.
 Proof.

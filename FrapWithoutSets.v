1bff5420e2a0ad962efa52560c7fbda43ebf1d47
Amanda Liu <lamanda@mit.edu>
2022-02-06 12:47:50 -0500

    Update cases tactic to work on N type

--- FrapWithoutSets.v
+++ FrapWithoutSets.v
@@ -229,6 +229,9 @@ Ltac cases E :=
   ((repeat match type of E with
            | _ \/ _ => destruct E as [E | E]
            end)
+   || (match type of E with
+       | N => destruct E using indN
+       end)
    || (is_var E; destruct E)
    || match type of E with
       | {_} + {_} => destruct E
